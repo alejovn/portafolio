114330cddee49cd1c145ced7cb9d9fe1e339c36b
alejo16 <alejandrovalencia2011@gmail.com>
2022-02-26 11:12:44 -0600

    acomodar habilidad

--- src/components/Section.js
+++ src/components/Section.js
@@ -15,12 +15,17 @@ const Section = (props) => {
         <span className="l-br">-</span>
         <h1>Habilidades</h1>
         <div className="row _fondo-section">
-          <div className="col-3">
+          <div className="col-4">
             <div className={"habilidades " + props.classNameH}>
-              <span className="l-br-s">-</span>
-              <h1>Java</h1>
-              <div className="img-habilidades">
-                <img className="" src="img/java.png" />
+              <div className="row">
+                <div className="col-6">
+                  <div className="img-habilidades">
+                    <img className="" src="img/java.png" />
+                  </div>
+                </div>
+                <div className="col-6">
+                  <h1>Java</h1>
+                </div>
               </div>
               <div className="codigo-java">
                 <ul>
@@ -41,29 +46,27 @@ const Section = (props) => {
               </div>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-4">
             <div className={"habilidades " + props.classNameH}>
               <span className="l-br-s">-</span>
               <div>
-                <h1>JavaScript</h1>
+                <h1>VB.net</h1>
               </div>
               <div className="img-habilidades">
-                <img className="" src="img/javascript.png" />
+                <img className="" src="img/vb.png" />
               </div>
               <div className="codigo-java">
                 <ul>
-                  <li>1 <span>{"<html>"}</span></li>
-                  <li>2 <span className="tap1">{"<body>"}</span></li>
-                  <li>3 <span className="tap2">{"<h1>Primer código JavaScript</h1>"}</span></li>
-                  <li>4 <span className="tap2">{"<script>"}</span></li>
-                  <li>5 <span className="tap3">{"document.write('Hola Mundo');"}</span></li>
-                  <li>6 <span className="tap2">{"</script>"}</span></li>
-                  <li>7 <span className="tap1">{"</body>"}</span></li>
-                  <li>8 <span>{"</html>"}</span></li>
+                  <li>1 <span>Module Module1</span></li>
+                  <li>2 <span className="tap1">Sub Main()</span></li>
+                  <li>3 <span className="tap2">System.Console.WriteLine("Hola Mundo")</span></li>
+                  <li>4 <span className="tap2">System.Console.ReadLine()</span></li>
+                  <li>5 <span className="tap1">End Sub</span></li>
+                  <li>6 <span>End Module</span></li>
                 </ul>
               </div>
               <div className="btn-div">
-                <a onClick={() => abrirModal()} class="ver-mas">
+                <a href="#" class="ver-mas">
                   <div>
                     <span></span>
                   </div>
@@ -72,7 +75,7 @@ const Section = (props) => {
               </div>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-4">
             <div className={"habilidades " + props.classNameH}>
               <span className="l-br-s">-</span>
               <div>
@@ -104,23 +107,58 @@ const Section = (props) => {
               </div>
             </div>
           </div>
-          <div className="col-3">
+        </div>
+        <div className="row _fondo-section">
+          <div className="col-4">
             <div className={"habilidades " + props.classNameH}>
               <span className="l-br-s">-</span>
               <div>
-                <h1>VB.net</h1>
+                <h1>JavaScript</h1>
               </div>
               <div className="img-habilidades">
-                <img className="" src="img/vb.png" />
+                <img className="" src="img/javascript.png" />
               </div>
               <div className="codigo-java">
                 <ul>
-                  <li>1 <span>Module Module1</span></li>
-                  <li>2 <span className="tap1">Sub Main()</span></li>
-                  <li>3 <span className="tap2">System.Console.WriteLine("Hola Mundo")</span></li>
-                  <li>4 <span className="tap2">System.Console.ReadLine()</span></li>
-                  <li>5 <span className="tap1">End Sub</span></li>
-                  <li>6 <span>End Module</span></li>
+                  <li>1 <span>{"<html>"}</span></li>
+                  <li>2 <span className="tap1">{"<body>"}</span></li>
+                  <li>3 <span className="tap2">{"<h1>Primer código JavaScript</h1>"}</span></li>
+                  <li>4 <span className="tap2">{"<script>"}</span></li>
+                  <li>5 <span className="tap3">{"document.write('Hola Mundo');"}</span></li>
+                  <li>6 <span className="tap2">{"</script>"}</span></li>
+                  <li>7 <span className="tap1">{"</body>"}</span></li>
+                  <li>8 <span>{"</html>"}</span></li>
+                </ul>
+              </div>
+              <div className="btn-div">
+                <a onClick={() => abrirModal()} class="ver-mas">
+                  <div>
+                    <span></span>
+                  </div>
+                  <p>Ver más</p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className={"habilidades " + props.classNameH}>
+              <span className="l-br-s">-</span>
+              <div>
+                <h1>Bootstrap</h1>
+              </div>
+              <div className="img-habilidades">
+                <img className="" src="img/bootstrap.png" />
+              </div>
+              <div className="codigo-java">
+                <ul>
+                  <li>1 <span>{"<html>"}</span></li>
+                  <li>2 <span className="tap1">{"<head>"}</span></li>
+                  <li>3 <span className="tap2">{"<link rel='stylesheet' href='bootstrap.min.css'>"}</span></li>
+                  <li>4 <span className="tap1">{"</head>"}</span></li>
+                  <li>5 <span className="tap1">{"<body>"}</span></li>
+                  <li>6 <span className="tap2">{"<button class='btn btn-dark'>Dark</button>"}</span></li>
+                  <li>7 <span className="tap1">{"</body>"}</span></li>
+                  <li>8 <span>{"</html>"}</span></li>
                 </ul>
               </div>
               <div className="btn-div">
@@ -133,9 +171,7 @@ const Section = (props) => {
               </div>
             </div>
           </div>
-        </div>
-        <div className="row _fondo-section">
-          <div className="col-3">
+          <div className="col-4">
             <div className={"habilidades " + props.classNameH}>
               <span className="l-br-s">-</span>
               <div>
@@ -154,16 +190,19 @@ const Section = (props) => {
                 </ul>
               </div>
               <div className="btn-div">
-                  <a href="#" class="ver-mas">
-                    <div>
-                      <span></span>
-                    </div>
-                    <p>Ver más</p>
-                  </a>
-                </div>
+                <a href="#" class="ver-mas">
+                  <div>
+                    <span></span>
+                  </div>
+                  <p>Ver más</p>
+                </a>
+              </div>
             </div>
           </div>
-          <div className="col-3">
+        </div>
+        <div className="row _fondo-section">
+
+          <div className="col-4">
             <div className={"habilidades " + props.classNameH}>
               <span className="l-br-s">-</span>
               <div>
@@ -185,49 +224,17 @@ const Section = (props) => {
                 </ul>
               </div>
               <div className="btn-div">
-                  <a href="#" class="ver-mas">
-                    <div>
-                      <span></span>
-                    </div>
-                    <p>Ver más</p>
-                  </a>
-                </div>
+                <a href="#" class="ver-mas">
+                  <div>
+                    <span></span>
+                  </div>
+                  <p>Ver más</p>
+                </a>
+              </div>
             </div>
           </div>
-          <div className="col-3">
-            <div className={"habilidades " + props.classNameH}>
-              <span className="l-br-s">-</span>
-              <div>
-                <h1>Bootstrap</h1>
-              </div>
-              <div className="img-habilidades">
-                <img className="" src="img/bootstrap.png" />
-              </div>
-              <div className="codigo-java">
-                <ul>
-                <li>1 <span>{"<html>"}</span></li>
-                  <li>2 <span className="tap1">{"<head>"}</span></li>
-                  <li>3 <span className="tap1">{"<link rel='stylesheet' href='bootstrap.css'>"}</span></li>
-                  <li>4 <span className="tap1">{"</head>"}</span></li>
-                  <li>5 <span className="tap1">{"<body>"}</span></li>
-                  <li>6 <span className="tap2">{"<button class='btn btn-dark'>"}</span></li>
-                  <li>7 <span className="tap3">{"Dark"}</span></li>
-                  <li>8 <span className="tap2">{"</button>"}</span></li>
-                  <li>9 <span className="tap1">{"</body>"}</span></li>
-                  <li>10 <span>{"</html>"}</span></li>
-                </ul>
-              </div>
-              <div className="btn-div">
-                  <a href="#" class="ver-mas">
-                    <div>
-                      <span></span>
-                    </div>
-                    <p>Ver más</p>
-                  </a>
-                </div>
-            </div>
-          </div>
-          <div className="col-3">
+
+          <div className="col-4">
             <div className={"habilidades " + props.classNameH}>
               <span className="l-br-s">-</span>
               <div>
@@ -238,54 +245,20 @@ const Section = (props) => {
               </div>
               <div className="codigo-java">
                 <ul>
-                  <li>1 <span>public class holaMundo{' {'}</span></li>
-                  <li>2 <span className="tap1">public static void main(String[] args){' {'}</span></li>
-                  <li>3 <span className="tap2">System.out.println("Hola Mundo");</span></li>
-                  <li>4 <span className="tap1">{'}'}</span></li>
-                  <li>5 <span>{'}'}</span></li>
+                  <li>1 <span>print("hello world")</span></li>
                 </ul>
               </div>
               <div className="btn-div">
-                  <a href="#" class="ver-mas">
-                    <div>
-                      <span></span>
-                    </div>
-                    <p>Ver más</p>
-                  </a>
-                </div>
+                <a href="#" class="ver-mas">
+                  <div>
+                    <span></span>
+                  </div>
+                  <p>Ver más</p>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row _fondo-section">
-          <div className="col-3">
-            <div className={"habilidades " + props.classNameH}>
-              <span className="l-br-s">-</span>
-              <div>
-                <h1>Android Studio</h1>
-              </div>
-              <div className="img-habilidades">
-                <img className="" src="img/angular.png" />
-              </div>
-              <div className="codigo-java">
-                <ul>
-                  <li>1 <span>public class holaMundo{' {'}</span></li>
-                  <li>2 <span className="tap1">public static void main(String[] args){' {'}</span></li>
-                  <li>3 <span className="tap2">System.out.println("Hola Mundo");</span></li>
-                  <li>4 <span className="tap1">{'}'}</span></li>
-                  <li>5 <span>{'}'}</span></li>
-                </ul>
-              </div>
-              <div className="btn-div">
-                  <a href="#" class="ver-mas">
-                    <div>
-                      <span></span>
-                    </div>
-                    <p>Ver más</p>
-                  </a>
-                </div>
-            </div>
-          </div>
-          <div className="col-3">
+          <div className="col-4">
             <div className={"habilidades " + props.classNameH}>
               <span className="l-br-s">-</span>
               <div>
@@ -296,76 +269,22 @@ const Section = (props) => {
               </div>
               <div className="codigo-java">
                 <ul>
-                  <li>1 <span>public class holaMundo{' {'}</span></li>
-                  <li>2 <span className="tap1">public static void main(String[] args){' {'}</span></li>
-                  <li>3 <span className="tap2">System.out.println("Hola Mundo");</span></li>
-                  <li>4 <span className="tap1">{'}'}</span></li>
-                  <li>5 <span>{'}'}</span></li>
+                  <li>1 <span>CREATE DATABASE Saludo;</span></li>
+                  <li>2 <span className="tap1">USE Saludo</span></li>
+                  <li>3 <span className="tap2">CREATE TABLE 'saludo' (</span></li>
+                  <li>4 <span className="tap1">{"'id' INT NOT NULL AUTO_INCREMENT, "}</span></li>
+                  <li>5 <span>{"'saludo' VARCHAR(50) NOT NULL, "}</span></li>
+                  <li>6 <span className="tap1">{"PRIMARY KEY ('id')"}</span></li>
+                  <li>7 <span>{")ENGINE = InnoDB;"}</span></li>
                 </ul>
               </div>
               <div className="btn-div">
-                  <a href="#" class="ver-mas">
-                    <div>
-                      <span></span>
-                    </div>
-                    <p>Ver más</p>
-                  </a>
-                </div>
-            </div>
-          </div>
-          <div className="col-3">
-            <div className={"habilidades " + props.classNameH}>
-              <span className="l-br-s">-</span>
-              <div>
-                <h1>SQL</h1>
-              </div>
-              <div className="img-habilidades">
-                <img className="" src="img/angular.png" />
-              </div>
-              <div className="codigo-java">
-                <ul>
-                  <li>1 <span>public class holaMundo{' {'}</span></li>
-                  <li>2 <span className="tap1">public static void main(String[] args){' {'}</span></li>
-                  <li>3 <span className="tap2">System.out.println("Hola Mundo");</span></li>
-                  <li>4 <span className="tap1">{'}'}</span></li>
-                  <li>5 <span>{'}'}</span></li>
-                </ul>
-              </div>
-              <div className="btn-div">
-                  <a href="#" class="ver-mas">
-                    <div>
-                      <span></span>
-                    </div>
-                    <p>Ver más</p>
-                  </a>
-                </div>
-            </div>
-          </div>
-          <div className="col-3">
-            <div className={"habilidades " + props.classNameH}>
-              <span className="l-br-s">-</span>
-              <div>
-                <h1>SQLite</h1>
-              </div>
-              <div className="img-habilidades">
-                <img className="" src="img/sqlite.png" />
-              </div>
-              <div className="codigo-java">
-                <ul>
-                  <li>1 <span>public class holaMundo{' {'}</span></li>
-                  <li>2 <span className="tap1">public static void main(String[] args){' {'}</span></li>
-                  <li>3 <span className="tap2">System.out.println("Hola Mundo");</span></li>
-                  <li>4 <span className="tap1">{'}'}</span></li>
-                  <li>5 <span>{'}'}</span></li>
-                </ul>
-              </div>
-              <div className="btn-div">
-                  <a href="#" class="ver-mas">
-                    <div>
-                      <span></span>
-                    </div>
-                    <p>Ver más</p>
-                  </a>
+                <a href="#" class="ver-mas">
+                  <div>
+                    <span></span>
+                  </div>
+                  <p>Ver más</p>
+                </a>
               </div>
             </div>
           </div>
