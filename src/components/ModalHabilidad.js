f16aa8d06558a25031103363c1fca41e4555a34a
alejo16 <alejandrovalencia2011@gmail.com>
2022-03-14 14:56:20 -0600

    modal

--- src/components/ModalHabilidad.js
+++ src/components/ModalHabilidad.js
@@ -1,11 +1,12 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Modal from 'react-modal';
-import React from 'react'
+import React from 'react';
+import ReactPlayer from 'react-player'
 const ModalHabilidad = (props) => {
     let subtitle;
     const customStyles = {
         content: {
-            backgroundColor: 'antiquewhite',
+            backgroundColor: 'transparent',
             top: '50%',
             left: '50%',
             right: 'auto',
@@ -25,21 +26,23 @@ const ModalHabilidad = (props) => {
     return (
         <div>
             <Modal
-                    isOpen={props.modalIsOpen}
-                    onAfterOpen={afterOpenModal}
-                    onRequestClose={closeModal}
-                    style={customStyles}
-                    contentLabel="Example Modal"
-                >
-                    <button className="btn-close" onClick={closeModal}><i className="fa fa-times"></i></button>
-                    <h2 ref={(_subtitle) => (subtitle = _subtitle)}></h2>
-                    <img src="img/cmd.png"/>
-                    <div className="decripcion-video">
-                        <p className="titulo-video">App punto de venta</p>
-                        <div className="linea-c"></div>
-                        <p>App android desarrollada en android studio con java, se uso SQLite para almacenar información.</p>
-                    </div>
-                </Modal>
+                isOpen={props.modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <button className="btn-close" onClick={closeModal}><i className="fa fa-times"></i></button>
+                <h2 ref={(_subtitle) => (subtitle = _subtitle)}></h2>
+                <ReactPlayer
+                    url='https://www.youtube.com/watch?v=HO0ME0yvbtI'
+                    className='react-player'
+                    controls
+                    width='100%'
+                    height='100%'
+                />
+            </Modal>
         </div>
     );
 }
